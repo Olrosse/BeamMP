e593ad63265e3af0a8992eb142786d3fbca4c3b7
Olrosse <10835721+Olrosse@users.noreply.github.com>
2024-09-19 00:40:28 +0200

    added basic damage variables sync

--- lua/vehicle/extensions/BeamMP/MPPowertrainVE.lua
+++ lua/vehicle/extensions/BeamMP/MPPowertrainVE.lua
@@ -14,6 +14,7 @@ local ignitionLevel
 local lastignitionLevel
 local spawnVehicleIgnitionLevel
 local devices = powertrain.getDevices()
+local combustionEngines = powertrain.getDevicesByType("combustionEngine")
 -- ============= VARIABLES =============
 
 
@@ -52,11 +53,14 @@ end
 
 
 local function getCombustionEngineData(data)
-	local combustionEngines = powertrain.getDevicesByType("combustionEngine")
 	if next(combustionEngines) then
 		local engines = {}
 		for engineName,engine in pairs(combustionEngines) do
 			local lastEngineData = lastCombustionEngineData[engineName] or {}
+			local storeOnly = false
+			if tableIsEmpty(lastEngineData) then
+				storeOnly = true
+			end
 			local engineData = {}
 			if lastEngineData.ignitionCoef ~= engine.ignitionCoef or lastEngineData.isStalled ~= engine.isStalled then
 				engineData.ign = engine.ignitionCoef
@@ -65,13 +69,60 @@ local function getCombustionEngineData(data)
 			if lastEngineData.starterEngagedCoef ~= engine.starterEngagedCoef then
 				engineData.starter = engine.starterEngagedCoef
 			end
+			if engine.friction ~= lastEngineData.friction and not engine.isBroken then
+				engineData.friction = engine.friction
+			end
+			if engine.outputTorqueState ~= lastEngineData.outputTorqueState then
+				engineData.outputTorqueState = engine.outputTorqueState
+			end
+			if engine.isDisabled ~= lastEngineData.isDisabled then
+				engineData.isDisabled = engine.isDisabled
+			end
+			if engine.isBroken ~= lastEngineData.isBroken then
+				engineData.isBroken = engine.isBroken
+			end
+			if engine.starterDisabled ~= lastEngineData.starterDisabled then
+				engineData.starterDisabled = engine.starterDisabled
+			end
+			if engine.thermals then
+				local thermals = engine.thermals
+				local coolantTemperature = math.floor(thermals.coolantTemperature)
+				if coolantTemperature ~= lastEngineData.coolantTemperature then
+					engineData.coolantTemperature = coolantTemperature
+				end
+				local engineBlockTemperature = math.floor(thermals.engineBlockTemperature)
+				if engineBlockTemperature ~= lastEngineData.engineBlockTemperature then
+					dump(engine.turbocharger:getPartCondition())
+					engineData.engineBlockTemperature = engineBlockTemperature
+				end
+				if thermals.engineBlockOverheatDamage ~= lastEngineData.engineBlockOverheatDamage then
+					engineData.engineBlockOverheatDamage = thermals.engineBlockOverheatDamage
+				end
+				local oilTemperature = math.floor(thermals.oilTemperature)
+				if oilTemperature ~= lastEngineData.oilTemperature then
+					engineData.oilTemperature = oilTemperature
+				end
+				if thermals.oilOverheatDamage ~= lastEngineData.oilOverheatDamage then
+					engineData.oilOverheatDamage = thermals.oilOverheatDamage
+				end
+				lastEngineData.coolantTemperature = coolantTemperature
+				lastEngineData.engineBlockTemperature = engineBlockTemperature
+				lastEngineData.engineBlockOverheatDamage = thermals.engineBlockOverheatDamage
+				lastEngineData.oilTemperature = oilTemperature
+				lastEngineData.oilOverheatDamage = thermals.oilOverheatDamage
+			end
 
 			lastEngineData.ignitionCoef = engine.ignitionCoef
 			lastEngineData.isStalled = engine.isStalled
 			lastEngineData.starterEngagedCoef = engine.starterEngagedCoef
+			lastEngineData.friction = engine.friction
+			lastEngineData.outputTorqueState = engine.outputTorqueState
+			lastEngineData.isDisabled = engine.isDisabled
+			lastEngineData.isBroken = engine.isBroken
+			lastEngineData.starterDisabled = engine.starterDisabled
 
 			lastCombustionEngineData[engineName] = lastEngineData
-			if next(engineData) then
+			if not storeOnly and next(engineData) then
 				engines[engine.name] = engineData
 			end
 		end
@@ -113,6 +164,40 @@ local function applyEngineData(data)
 			end
 			if engineData.isStalled ~= nil then
 				receivedEngineData.isStalled = engineData.isStalled
+			end
+			local device = powertrain.getDevice(engineName)--combustionEngines[engineName]
+			if device then
+				if engineData.outputTorqueState ~= nil then
+					device.outputTorqueState = engineData.outputTorqueState
+					damageTracker.setDamage("engine", "engineReducedTorque", device.outputTorqueState < 1)
+				end
+				if engineData.friction ~= nil then
+					device.friction = engineData.friction
+				end
+				if engineData.coolantTemperature ~= nil then
+					device.thermals.coolantTemperature = engineData.coolantTemperature
+				end
+				if engineData.engineBlockTemperature ~= nil then
+					device.thermals.engineBlockTemperature = engineData.engineBlockTemperature
+				end
+				if engineData.engineBlockOverheatDamage ~= nil then
+					device.thermals.engineBlockOverheatDamage = engineData.engineBlockOverheatDamage
+				end
+				if engineData.oilTemperature ~= nil then
+					device.thermals.oilTemperature = engineData.oilTemperature
+				end
+				if engineData.oilOverheatDamage ~= nil then
+					device.thermals.oilOverheatDamage = engineData.oilOverheatDamage
+				end
+				if engineData.isDisabled ~= nil then
+					device.isDisabled = engineData.isDisabled
+				end
+				if engineData.isBroken ~= nil then
+					device.isBroken = engineData.isBroken
+				end
+				if engineData.starterDisabled ~= nil then
+					device.starterDisabled = engineData.starterDisabled
+				end
 			end
 			cachedCombustionEngineData[engineName] = receivedEngineData
 		end
