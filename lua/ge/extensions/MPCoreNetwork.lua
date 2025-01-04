2286fcb52672b486a9ea94af64d8a1025a12f9d7
Starystars67 <starystars67@gmail.com>
2025-01-04 20:55:48 +0000

    4.13.12

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -22,7 +22,7 @@ local launcherConnected = false
 local isConnecting = false
 local proxyPort = ""
 local launcherVersion = "" -- used only for the server list
-local modVersion = "4.13.11" -- the mod version
+local modVersion = "4.13.12" -- the mod version
 -- server
 
 local serverList -- server list JSON
